29a150a456de8991a27da2f725d80eff96d22253
Ryan Gravener <rgravener@wolf-matrix.local>
2012-02-26 18:15:04 -0500

    ios

---
+++ ios/NJ RailTests/NJ_RailTests.h
@@ -0,0 +1,13 @@
+//
+//  NJ_RailTests.h
+//  NJ RailTests
+//
+//  Created by Mark Novak on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NJ_RailTests : SenTestCase
+
+@end
---
+++ ios/NJ RailTests/NJ_RailTests.m
@@ -0,0 +1,32 @@
+//
+//  NJ_RailTests.m
+//  NJ RailTests
+//
+//  Created by Mark Novak on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NJ_RailTests.h"
+
+@implementation NJ_RailTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NJ RailTests");
+}
+
+@end
